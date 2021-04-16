1f355b37407a726224e19c61ba0621e79d66d0e2
natal <drossel.fb@gmail.com>
2021-04-16 17:26:19 +0900

    Getエンドポイント test 追加

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ async function runServer() {
 server.use(bodyParser.json());
 server.use('/api/v1/portfolios', require('./routes/portfolios'));
 
+
+server.get('/test', (req, res) => {
+  res.json({ message: 'Hello World' });
+})
+
 const PORT = parseInt(process.env.PORT, 10) || 3001;
 server.listen(PORT, (err) => {
   if (err) console.error(err);
